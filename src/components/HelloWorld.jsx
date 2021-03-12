0517c64ee94ad4705752cb4277e24ed6784d7a3c
andleo17 <andres.gastulo@gmail.com>
2021-03-12 16:45:06 -0500

    Config ready

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const HelloWorld = () => {
+	return <h1>Hola mundo</h1>;
+};
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+
+const app = <HelloWorld />;
+
+ReactDOM.render(app, document.getElementById('app'));
